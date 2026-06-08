e8360c445bc2b578bc244a7e30ab069e5208c3cf
Tomas Perez Sanchez <tomasperez0899@gmail.com>
2026-06-08 21:24:41 +0100

    summary plots

--- Main_project/src/pipelines/4_Models/JSON_generator_DTandRF.py
+++ Main_project/src/pipelines/4_Models/JSON_generator_DTandRF.py
@@ -15,7 +15,7 @@ from datetime import datetime
 # ============================================================
 
 USER_INFO = {
-    "patient_id": "XB47Y",
+    "patient_id": "JYXFE",
     "experiment_name": "DT_RF_models",
     "version": "v01"
 }
@@ -44,12 +44,12 @@ INPUTS = {
         / "results"
         / PATIENT_ID
         / "Feature_ext"
-        / "Part2_features"
-        / "XB47Y_IN-normalized_npz_FP-fullnpz_W10s_PRE6to5min_ICT0to1min_GAPasINT_FINAL-PREvsSEIZ_20260504_v01_FEAT-TIME-FREQ_20260505_v01"
+        / "Part3_PCA"
+        / "JYXFE_JYXFE_IN-normalized_npz_FP-fullnpz_W10s_PRE6to5min_ICT0to1min_GAPasINT_FINAL-PREvsSEIZ_20260511_v01_FEAT-TIME-FREQ_20260511_v01_df_features_ictalVspreictal_PCA_VAR90_20260608_v01"
     ),
 
     # Input dataframe filename
-    "input_filename": "df_windowsXB47Y_pca.pkl",
+    "input_filename": "JYXFE_JYXFE_IN-normalized_npz_FP-fullnpz_W10s_PRE6to5min_ICT0to1min_GAPasINT_FINAL-PREvsSEIZ_20260511_v01_FEAT-TIME-FREQ_20260511_v01_df_features_ictalVspreictal_PCA_VAR90_20260608_v01_df_windows_pca.pkl",
 
     # Input data type for automatic naming.
     # Options:
